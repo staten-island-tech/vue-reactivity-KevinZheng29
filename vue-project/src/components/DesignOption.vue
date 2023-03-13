09aa779c5394493fec80dc20a5e721c4a2dfe482
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-03-13 14:00:11 -0400

    1

--- vue-project/src/components/DesignOption.vue
+++ vue-project/src/components/DesignOption.vue
@@ -4,7 +4,7 @@
       v-on:click="test"
       class="img"
       v-for="image in characterlink"
-      :src="image"
+      :src="image.IMG"
       :alt="image.alt"
     />
   </div>
@@ -14,13 +14,19 @@
 export default {
   data() {
     return {
-      test: function () {
-        console.log('hi')
-      },
       characterlink: [
-        'https://th.bing.com/th/id/R.bb75d09bce90bd1eef9b69fc34cb02b3?rik=YgLpsYw65dCbsg&riu=http%3a%2f%2fmedia.giphy.com%2fmedia%2f19z865XUHqTcs%2fgiphy.gif&ehk=fpm2SWYOpP18S0CxqjtBgqkgGRNMg2qWR9W5nYK7Qmc%3d&risl=&pid=ImgRaw&r=0',
-        'https://orig00.deviantart.net/2ec1/f/2016/310/2/1/link_pixel_art_32_bit_by_knightbruh-dampj2t.gif',
-        'https://thumbs.gfycat.com/FaintSkinnyFowl-max-1mb.gif'
+        {
+          Character: 1,
+          IMG: 'https://th.bing.com/th/id/R.bb75d09bce90bd1eef9b69fc34cb02b3?rik=YgLpsYw65dCbsg&riu=http%3a%2f%2fmedia.giphy.com%2fmedia%2f19z865XUHqTcs%2fgiphy.gif&ehk=fpm2SWYOpP18S0CxqjtBgqkgGRNMg2qWR9W5nYK7Qmc%3d&risl=&pid=ImgRaw&r=0'
+        },
+        {
+          Character: 2,
+          IMG: 'https://orig00.deviantart.net/2ec1/f/2016/310/2/1/link_pixel_art_32_bit_by_knightbruh-dampj2t.gif'
+        },
+        {
+          Character: 3,
+          IMG: 'https://thumbs.gfycat.com/FaintSkinnyFowl-max-1mb.gif'
+        }
       ]
     }
   }
